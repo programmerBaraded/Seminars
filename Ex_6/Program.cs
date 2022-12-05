86ebbb28f4e2feb9fed525d977a4255cc762e4bf
Baraded <borodeda@gmail.com>
2022-12-05 15:55:03 +0300

    done ex6

--- Ex_6/Program.cs
+++ Ex_6/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// программу, которая на вход принимает число и выдаёт, является ли число чётным
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int n = int.Parse(Console.ReadLine());
+
+if (n % 2 == 0)
+    Console.WriteLine($" {n}  четное число");
+else
+    Console.WriteLine($" {n} нечетное число");
