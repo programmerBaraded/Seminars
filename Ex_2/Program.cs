e9259887c0165156ea83ecfb2967885a37b8c826
Baraded <borodeda@gmail.com>
2022-12-05 13:59:10 +0300

    done ex_2

---
+++ Ex_2/Program.cs
@@ -0,0 +1,15 @@
+﻿// программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее
+Console.Clear ();
+Console.WriteLine("Введите первое число: ");
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int m = int.Parse(Console.ReadLine());
+
+if (n > m)
+Console.WriteLine($"max {n}  min {m}");
+
+else if (n < m)
+Console.WriteLine($"max {m}  min {n}");
+
+else
+Console.WriteLine("Числа ровны");
