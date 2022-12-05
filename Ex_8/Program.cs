1f85586bbb527ce733b4e1dcd96183515db56e64
Baraded <borodeda@gmail.com>
2022-12-05 18:17:27 +0300

    done ex8

--- Ex_8/Program.cs
+++ Ex_8/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N
+
+Console.Clear();
+
+Console.WriteLine("Введите число: ");
+int N = int.Parse(Console.ReadLine());
+for (int i = 1; i <= N; i++)
+    if (i % 2 == 0)
+        Console.WriteLine(i);
