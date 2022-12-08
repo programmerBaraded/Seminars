b704543e23cabbb7250048deda5c48c8866c9d53
Baraded <borodeda@gmail.com>
2022-12-08 20:28:03 +0300

    done ex13&ex15

--- Ex_13/Program.cs
+++ Ex_13/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// программу, которая выводит третью цифру заданного числа 
+//или сообщает, что третьей цифры нет
+
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int n = int.Parse(Console.ReadLine()); //код работает до момента привышения критического размера памяти
+if (n < 0)
+{
+    int k = n * -1;
+    n = k;
+}
+while (n > 1000)
+{
+ int n1 = n / 10;
+ n = n1;
+}
+if (n > 99)
+{
+    //int n1 = n % 10;
+    Console.WriteLine((n % 100) % 10); //n1
+}
+else
+{
+    Console.WriteLine("третьей цифры нет");
+}
--- Ex_15/Program.cs
+++ Ex_15/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным
+Console.Clear();
+Console.Write("Введи цифру, обозначающую день недели: ");
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+
+void CheckingDay (int dayNumber) 
+{
+  if (dayNumber == 6 || dayNumber == 7) 
+  {
+  Console.WriteLine("(этот день выходной) -> да");
+  }
+  else if (dayNumber < 1 || dayNumber > 7) 
+  {
+    Console.WriteLine("это вообще не день недели");
+  }
+  else Console.WriteLine("(этот день не выходной) -> нет");
+}
+
+CheckingDay(dayNumber);
