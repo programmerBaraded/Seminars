6cb8e7f2a929b1ed2a421a84e24dea2c5721dd22
Baraded <borodeda@gmail.com>
2022-12-08 07:08:07 +0300

    done ex10

--- Ex_10/Program.cs
+++ Ex_10/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа
+
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int n = int.Parse(Console.ReadLine());
+int n1 = n % 100;
+int n2 = n1 / 10;
+Console.WriteLine(n2);
