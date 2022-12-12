01572c9538ab45fea36a831064445c789df4d499
Baraded <borodeda@gmail.com>
2022-12-12 15:47:19 +0300

    done ex23

--- Ex_23/Program.cs
+++ Ex_23/Program.cs
@@ -2,22 +2,6 @@
 
 Console.Clear();
 Console.Write("Введите число: ");
-int n = int.Parse(Console.ReadLine());
-string s = n.ToString();
-int x = 0;
-for (int i = 0; i < s.Length / 2; i++)
-{
-    if (s[i] == s[s.Length - 1 - i])
-    {
-       x++; 
-    }
-    
-}
-if (x == s.Length / 2)
-{
-    Console.WriteLine("палиндром");
-}
-else
-{
-    Console.WriteLine("не палиндром");
-}
+int N = int.Parse(Console.ReadLine());
+for (int i = 1; i <= N; i++)
+    Console.Write($"{i * i * i}  ");
