a158a9fa1cea5ac4cc2a9d801c7636efa4d2b57b
Baraded <borodeda@gmail.com>
2022-12-12 15:29:18 +0300

    done ex21

--- Ex_21/Program.cs
+++ Ex_21/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// программa, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве
+
+Console.Clear();
+Console.Write("Введите X первой точки: ");
+double x1 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите Y первой точки: ");
+double y1 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите Z первой точки: ");
+double z1 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите X второй точки: ");
+double x2 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите Y второй точки: ");
+double y2 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите Z второй точки: ");
+double z2 = Convert.ToDouble(Console.ReadLine());
+
+double dist = Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2) + Math.Pow(z1 - z2, 2));
+
+Console.WriteLine(Math.Round(dist, 2));
--- Ex_23/Program.cs
+++ Ex_23/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// программa, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N
+
+Console.Clear();
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine());
+string s = n.ToString();
+int x = 0;
+for (int i = 0; i < s.Length / 2; i++)
+{
+    if (s[i] == s[s.Length - 1 - i])
+    {
+       x++; 
+    }
+    
+}
+if (x == s.Length / 2)
+{
+    Console.WriteLine("палиндром");
+}
+else
+{
+    Console.WriteLine("не палиндром");
+}
