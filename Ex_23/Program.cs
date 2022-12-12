2131d03eebe83dd5446cc6ec00562f3d1025c8de
Baraded <borodeda@gmail.com>
2022-12-12 15:52:53 +0300

    код получше

--- Ex_23/Program.cs
+++ Ex_23/Program.cs
@@ -4,4 +4,5 @@ Console.Clear();
 Console.Write("Введите число: ");
 int N = int.Parse(Console.ReadLine());
 for (int i = 1; i <= N; i++)
-    Console.Write($"{i * i * i}  ");
+    // Console.Write($"{i * i * i}  ");
+    Console.Write($"{Math.Pow(i, 3)}  ");
