0c836bf521110086cc79bbd83b091192870c60d1
Baraded <borodeda@gmail.com>
2022-12-15 22:03:52 +0300

    done ex25

--- Ex_25/Program.cs
+++ Ex_25/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// программa, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// !!! Нельзя использовать функцию Math.Pow
+
+Console.Clear();
+Console.Write("Введите число A: ");
+int A = int.Parse(Console.ReadLine());
+Console.Write("Введите число B: ");
+int B = int.Parse(Console.ReadLine());
+int resalt = A;
+for (int i = 1; i < B; i++)
+{
+    resalt = resalt * A;
+}
+Console.Write(resalt);
