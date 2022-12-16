6efa28324b28b6d5f231966c90359e9824dd1389
Baraded <borodeda@gmail.com>
2022-12-16 06:55:46 +0300

    done ex29

--- Ex_29/Program.cs
+++ Ex_29/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// программa, которая спрашивает у пользователя кол-во элементов и спршивает сами элементы. 
+// После записи элементов в массив, необходимо его вывести на экран.
+
+Console.Clear();
+Console.Write("Введите кол-во элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+Console.Write("Введите нижний предел массива: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите верхний предел массива: ");
+int max = Convert.ToInt32(Console.ReadLine());
+for (int i = 0; i < array.Length; i++)
+array[i] = new Random().Next(min, max);
+
+Console.WriteLine($"[{string.Join(", ", array)}]");
