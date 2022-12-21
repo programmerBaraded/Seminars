45e9c6ad2eda52c7a4059b7a9845094ff9fc946b
Baraded <borodeda@gmail.com>
2022-12-21 04:51:55 +0300

    done 41

---
+++ Ex_41/Program.cs
@@ -0,0 +1,10 @@
+﻿// Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+
+int[] s = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+int p = 0;
+foreach (int element in s)
+    if (element > 0)
+        p++;
+Console.WriteLine(p);
