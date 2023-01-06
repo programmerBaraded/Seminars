c8d6c8515355d4e0626e233b2e15e516a880dbd4
Baraded <borodeda@gmail.com>
2023-01-06 13:30:17 +0300

    done ex58_var3

---
+++ Ex_58_var2/Program.cs
@@ -0,0 +1,57 @@
+﻿// Задача 58 (правильно по алгоритму) https://programm.top/c-sharp/programs/matrix-multiplication/
+// Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+
+void InputMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            matrix[i, j] = new Random().Next(1, 4);
+    }
+}
+
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            Console.Write($"{matrix[i, j]} \t");
+        Console.WriteLine();
+    }
+}
+
+void MultiplyMatrix(int[,] firstMartrix, int[,] secomdMartrix, int[,] resultMatrix)
+{
+  for (int i = 0; i < resultMatrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < resultMatrix.GetLength(1); j++)
+    {
+      int sum = 0;
+      for (int k = 0; k < firstMartrix.GetLength(1); k++)
+      {
+        sum += firstMartrix[i,k] * secomdMartrix[k,j];
+      }
+      resultMatrix[i,j] = sum;
+    }
+  }
+}
+
+Console.Clear();
+Console.Write("Введите размеры матрицы: ");
+int[] size = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+int[,] firstMartrix = new int[size[0], size[1]];
+InputMatrix(firstMartrix);
+Console.WriteLine($"\nПервая матрица:");
+PrintMatrix(firstMartrix);
+
+int[,] secomdMartrix = new int[size[0], size[1]];
+InputMatrix(secomdMartrix);
+Console.WriteLine($"\nВторая матрица:");
+PrintMatrix(secomdMartrix);
+
+int[,] resultMatrix = new int[size[0], size[1]];
+
+MultiplyMatrix(firstMartrix, secomdMartrix, resultMatrix);
+Console.WriteLine($"\nПроизведение первой и второй матриц:");
+PrintMatrix(resultMatrix);
---
+++ Ex_58_var3/Program.cs
@@ -0,0 +1,70 @@
+﻿// Задача 58 (правильно по алгоритму) https://programm.top/c-sharp/programs/matrix-multiplication/
+// Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+
+int InputNumbers(string input)
+{
+  Console.Write(input);
+  int output = Convert.ToInt32(Console.ReadLine());
+  return output;
+}
+
+void InputMatrix(int[,] matrix)
+{
+  for (int i = 0; i < matrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+      matrix[i, j] = new Random().Next(1, 4);
+    }
+  }
+}
+
+void PrintMatrix (int[,] matrix)
+{
+  for (int i = 0; i < matrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+      Console.Write($"{matrix[i,j]} \t");  //+ " "
+    }
+    Console.WriteLine();
+  }
+}
+
+void MultiplyMatrix(int[,] firstMartrix, int[,] secomdMartrix, int[,] resultMatrix)
+{
+  for (int i = 0; i < resultMatrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < resultMatrix.GetLength(1); j++)
+    {
+      int sum = 0;
+      for (int k = 0; k < firstMartrix.GetLength(1); k++)
+      {
+        sum += firstMartrix[i,k] * secomdMartrix[k,j];
+      }
+      resultMatrix[i,j] = sum;
+    }
+  }
+}
+
+Console.Clear();
+Console.WriteLine($"\nВведите размеры матриц и диапазон случайных значений:");
+int m = InputNumbers("Введите число строк 1-й матрицы: ");
+int n = InputNumbers("Введите число столбцов 1-й матрицы (и строк 2-й): ");
+int p = InputNumbers("Введите число столбцов 2-й матрицы: ");
+
+int[,] firstMartrix = new int[m, n];
+InputMatrix(firstMartrix);
+Console.WriteLine($"\nПервая матрица:");
+PrintMatrix(firstMartrix);
+
+int[,] secomdMartrix = new int[n, p];
+InputMatrix(secomdMartrix);
+Console.WriteLine($"\nВторая матрица:");
+PrintMatrix(secomdMartrix);
+
+int[,] resultMatrix = new int[m,p];
+
+MultiplyMatrix(firstMartrix, secomdMartrix, resultMatrix);
+Console.WriteLine($"\nПроизведение первой и второй матриц:");
+PrintMatrix(resultMatrix);
